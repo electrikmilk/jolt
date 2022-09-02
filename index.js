d07eea75f7f03f55200bd5c463fddfab93689ff5
electrikmilk <brandonjordan124@gmail.com>
2022-09-02 00:17:49 -0400

    Use Jolt

---
+++ index.js
@@ -0,0 +1,26 @@
+import * as Jolt from 'jolt';
+
+Jolt.create({
+	ready: function() {
+		console.log('ready!');
+	},
+	data: {
+		docs: 'https://github.com/electrikmilk/jolt/wiki',
+		doList: [
+			'Be easy to learn',
+			'Keep things simple',
+			'Memorable APIs',
+			'Create abstractions that make life easier for the developer',
+			'Use dependencies only when absolutely necessary',
+		],
+		dontList: [
+			'Add unnecessary bloat that a few devs will use (DIY!)',
+			'Create the next fully featured framework for huge websites',
+			'Create unnecessarily complex abstractions, APIs, and tools',
+			'Add bloating, unnecessary, needlessly complex dependencies',
+		]
+	},
+	functions: {
+
+	}
+});
